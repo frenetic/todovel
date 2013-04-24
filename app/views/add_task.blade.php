d4925520187548aca16c716e9783e4ef33515f2b
Guilherme Medeiros <contato@frenetic.com.br>
2013-04-24 16:19:28 -0300

    Creating task view

---
+++ app/views/add_task.blade.php
@@ -0,0 +1,25 @@
+@extends('template')
+
+@section('conteudo')
+
+    <div class="row-fluid marketing">
+        <div class="span6">
+            @if ( count($errors) > 0)
+                Erros encontrados:<br />
+                <ul>
+                    @foreach ($errors->all() as $e)
+                        <li>{{ $e }}</li>
+                    @endforeach
+                </ul>
+            @endif
+            
+            {{ Form::open( array("action" => "TaskController@postAdd") ) }}
+                {{ Form::label('titulo', 'Tarefa a ser cumprima:') }}
+                    {{ Form::text('titulo') }}
+                
+                {{ Form::submit('OK') }}
+            {{ Form::close() }}
+            
+        </div>
+    </div>
+@stop
