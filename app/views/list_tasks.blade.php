e6aca1feb1a57905366a8ca1ddabd8c3f3c8e37f
Guilherme Medeiros <contato@frenetic.com.br>
2013-04-25 00:27:57 -0300

    Updating layout and adding jquery for ajax requests

--- app/views/list_tasks.blade.php
+++ app/views/list_tasks.blade.php
@@ -2,9 +2,43 @@
 
 @section('conteudo')
     
-    <ul>
+    <ul style="list-style: none;">
         @foreach ($tasks as $task)
-            <li>{{ $task->titulo }}</li>
+            <li class="task">
+                @if ($task->status)
+                    <span class="label label-success">{{ $task->titulo }}</span>
+                @else
+                    <label data-task-id='{{ $task->id }}'>
+                        <input type="checkbox" />
+                        {{ $task->titulo }}
+                    </label>
+                @endif
+            </li>
         @endforeach
     </ul>
+@stop
+
+
+@section('custom_script')
+    <script language="javascript">
+        $(document).ready( function() {
+
+            $('li label input').on('change', function(){
+                var task_id = $(this).parent().data('task-id');
+                var li = $(this).parent().parent();
+
+                //ajax post request
+                $.post(
+                    "/task/check",
+                    {task_id: task_id},
+                    function(data) {
+                        //callback do ajax request
+                        if (data.status == true) {
+                            li.html("<span class='label label-success'>"+data.titulo+"</span>");
+                        }
+                    }
+                );
+            });
+        });
+    </script>
 @stop
