98479ef953ced48c2ab2db050dbc36adcd47114f
Guilherme Medeiros <contato@frenetic.com.br>
2013-04-25 00:28:54 -0300

    Creating CHECK method to update task status through ajax

--- app/controllers/TaskController.php
+++ app/controllers/TaskController.php
@@ -6,17 +6,17 @@ class TaskController extends BaseController {
 	}
 	
 	public function postAdd() {
-		//criando regras de validação
+		//criando regras de validaÃ§Ã£o
 		$regras = array('titulo' => 'required');
 		
-		//executando validação
+		//executando validaÃ§Ã£o
 		$validacao = Validator::make(Input::all(), $regras);
 		
-		//se a validação deu errado
+		//se a validaÃ§Ã£o deu errado
 		if ($validacao->fails()) {
 			return Redirect::to('task/add')->withErrors($validacao);
 		}
-		//se a validação deu certo
+		//se a validaÃ§Ã£o deu certo
 		else {
 			$task = new Task;
 			$task->titulo = Input::get('titulo');
@@ -29,4 +29,32 @@ class TaskController extends BaseController {
 	public function listar() {
 		return View::make('list_tasks')->with('tasks', Task::all());
 	}
+
+    public function check() {
+        //verifica se a request Ã© ajax
+        if (Request::ajax()) {
+            //criando regras de validaÃ§Ã£o
+            $regras = array('task_id' => 'required|integer');
+
+            $validacao = Validator::make(Input::all(), $regras);
+
+            if ($validacao->fails()) {
+                return Response::json( array("status" => FALSE) );
+            }
+            else {
+                //tenta encontrar e atualizar a task
+                try {
+                    $task = Task::findOrFail(Input::get('task_id'));
+                    $task->status = TRUE;
+                    $task->save();
+
+                    return Response::json( array("status" => TRUE, "titulo" => $task->titulo) );
+                }
+                //caso nÃ£o tenha conseguido encontrar a task
+                catch(Exception $e) {
+                    return Response::json( array("status" => FALSE, "mensagem" => $e->getMessage()) );
+                }
+            }
+        }
+    }
 }
