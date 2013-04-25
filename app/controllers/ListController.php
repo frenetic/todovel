bd0f344eeac4f750280221f256b425b49023d100
Guilherme Medeiros <contato@frenetic.com.br>
2013-04-25 18:15:36 -0300

    Creating ListController

---
+++ app/controllers/ListController.php
@@ -0,0 +1,29 @@
+<?php
+
+class ListController extends BaseController {
+	public function getCreate() {
+		return View::make('add_list');
+	}
+	
+	
+	public function postCreate() {
+		//criando regras de validação
+		$regras = array('titulo' => 'required');
+		
+		//executando validação
+		$validacao = Validator::make(Input::all(), $regras);
+		
+		//se a validação deu errado
+		if ($validacao->fails()) {
+			return Redirect::to('list/create')->withErrors($validacao);
+		}
+		//se a validação deu certo
+		else {
+			$list = new Lista;
+			$list->titulo = Input::get('titulo');
+			$list->save();
+			
+			return View::make('add_list')->with('sucesso', TRUE);
+		}
+	}
+}
