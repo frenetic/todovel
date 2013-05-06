686624e8b11eaa8fe21c3c832ff393afe35047fb
Guilherme Medeiros <contato@frenetic.com.br>
2013-05-06 17:48:26 -0300

    Refactoring the tasks

--- app/controllers/TaskController.php
+++ app/controllers/TaskController.php
@@ -3,14 +3,16 @@
 class TaskController extends BaseController {
 	public function getAdd($lista_id) {
 		//verifica se a lista existe
-		Lista::findOrFail($lista_id);
+		if ( Lista::findOrFail($lista_id)->usuario->id != Auth::user()->id )
+			return Redirect::to('list');
 		
 		return View::make('add_task')->with('lista_id', $lista_id);
 	}
 	
 	public function postAdd($lista_id) {
 		//verifica se a lista existe
-		Lista::findOrFail($lista_id);
+		if ( Lista::findOrFail($lista_id)->usuario->id != Auth::user()->id )
+			return Redirect::to('list');
 		
 		
 		//criando regras de validação
@@ -37,32 +39,37 @@ class TaskController extends BaseController {
 	public function listar() {
 		return View::make('list_tasks')->with('tasks', Task::all());
 	}
-
-    public function check() {
-        //verifica se a request é ajax
-        if (Request::ajax()) {
-            //criando regras de validação
-            $regras = array('task_id' => 'required|integer');
-
-            $validacao = Validator::make(Input::all(), $regras);
-
-            if ($validacao->fails()) {
-                return Response::json( array("status" => FALSE) );
-            }
-            else {
-                //tenta encontrar e atualizar a task
-                try {
-                    $task = Task::findOrFail(Input::get('task_id'));
-                    $task->status = TRUE;
-                    $task->save();
-
-                    return Response::json( array("status" => TRUE, "titulo" => $task->titulo) );
-                }
-                //caso não tenha conseguido encontrar a task
-                catch(Exception $e) {
-                    return Response::json( array("status" => FALSE, "mensagem" => $e->getMessage()) );
-                }
-            }
-        }
-    }
+	
+	
+	public function check() {
+		//verifica se a request é ajax
+		if (Request::ajax()) {
+			//criando regras de validação
+			$regras = array('task_id' => 'required|integer');
+			
+			$validacao = Validator::make(Input::all(), $regras);
+			
+			if ($validacao->fails()) {
+				return Response::json( array("status" => FALSE) );
+			}
+			else {
+				//tenta encontrar e atualizar a task
+				try {
+					$task = Task::findOrFail(Input::get('task_id'));
+					
+					if ($task->getUsuarioId() != Auth::user()->id)
+						throw new Exception("você não é o dono dessa task");
+					
+					$task->status = TRUE;
+					$task->save();
+					
+					return Response::json( array("status" => TRUE, "titulo" => $task->titulo) );
+				}
+				//caso não tenha conseguido encontrar a task
+				catch(Exception $e) {
+					return Response::json( array("status" => FALSE, "mensagem" => $e->getMessage()) );
+				}
+			}
+		}
+	}
 }
--- app/models/Task.php
+++ app/models/Task.php
@@ -4,4 +4,8 @@ class Task extends Eloquent {
     public function lista() {
         return $this->belongsTo('Lista', 'list_id');
     }
+    
+    public function getUsuarioId() {
+	return $this->lista->usuario->id;
+    }
 }
